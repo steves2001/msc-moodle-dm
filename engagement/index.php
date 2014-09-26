63b1447e7e58601f0447c24f51b90c2313433b28
steves2001 <moodle@stephensmith.me.uk>
2014-09-26 13:29:17 +0000

    Added the basic structure for a Moodle report
    Duplicated a report from the Moodle code and removed all extra files.  Code is not currently executable and should not be uploaded to Moodle at this point.  Requires as a minimum the development of the index.php page and the report_engagement.php needs to be written.

---
+++ engagement/index.php
@@ -0,0 +1,12 @@
+<?php
+// This file is part of the engan#gment reporting tool
+//  
+//  Version 0.1a
+//  
+//  Author: Stephen Smith
+//  
+//  you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
