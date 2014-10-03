4dcf6b7a8b222cd4627dbabb60b14f3fcc226461
steves2001 <moodle@stephensmith.me.uk>
2014-10-03 14:09:11 +0000

    Added security code to the index.php file
    Users must now log in to moodle to use the page.
    Users must now have the correct permissions to view the page.

--- engagement/index.php
+++ engagement/index.php
@@ -1,7 +1,7 @@
 <?php
 // This file is part of the engan#gment reporting tool
 //  
-//  Version 0.1a
+//  Version 0.4a
 //  
 //  Author: Stephen Smith
 //  
@@ -13,10 +13,42 @@
 require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 
+//  Authenticate before using the page
+$id          = optional_param('id', 0, PARAM_INT);// Course ID
+$host_course = optional_param('host_course', '', PARAM_PATH);// Course ID
+
+if (empty($host_course)) {
+    $hostid = $CFG->mnet_localhost_id;
+    if (empty($id)) {
+        $site = get_site();
+        $id = $site->id;
+    }
+} else {
+    list($hostid, $id) = explode('/', $host_course);
+}
+
+
+if ($hostid == $CFG->mnet_localhost_id) {
+    $course = $DB->get_record('course', array('id'=>$id), '*', MUST_EXIST);
+
+} else {
+    $course_stub       = $DB->get_record('mnet_log', array('hostid'=>$hostid, 'course'=>$id), '*', true);
+    $course->id        = $id;
+    $course->shortname = $course_stub->coursename;
+    $course->fullname  = $course_stub->coursename;
+}
+
+
+require_login($course);
+
+$context = context_course::instance($course->id);
+
+require_capability('report/engagement:view', $context);
+
 // Display the report
 echo $OUTPUT->header();
 echo $OUTPUT->heading("Hello World");
 echo $OUTPUT->box_start();
-
+// Do some clever stuff here!!
 echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
--- engagement/version.php
+++ engagement/version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014100300;    // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014100301;    // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;    // Requires this Moodle version
 $plugin->component = 'report_engagement';  // Full name of the plugin (used for diagnostics)
